cf9afc358966647090d809d1c7b1d23decfd3610
owen danke <dankeowen@gmail.com>
2026-02-04 23:06:45 -0500

    Changed modalBottomSheet to showBottomSheet.

--- lib/models/poi_node.dart
+++ lib/models/poi_node.dart
@@ -63,17 +63,80 @@ class PoiNode {
   }
 
   static Marker build(BuildContext context, PointOfInterest poi, Function(int, {String? routeName})? onTabChange) {
+    final Color markerColor = Colors.orange;
     return Marker(
         point: poi.location,
         width: 40,
         height: 40,
         child: GestureDetector(
-          onTap:() => _showDetails(context, poi, onTabChange),
+          //onTap:() => _showDetails(context, poi, onTabChange),
+          onTap:() => showBottomSheet(
+            context: context, 
+            enableDrag: true,
+            showDragHandle: true,
+            builder: (BuildContext context) {
+              return IntrinsicHeight(
+                //height: (MediaQuery.of(context).size.width) / 3,
+                //width: (MediaQuery.of(context).size.width),
+                //padding: const EdgeInsets.all(16),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      // Title
+                      Padding(
+                        padding: EdgeInsetsGeometry.directional(start: 8, end: 8, bottom: 8),
+                        child: Text(
+                          '${poi.name} (${poi.id})',
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      
+                      // layout tree info and close buttons in a row
+                      Row(
+                        children: [
+                          // Learn more button
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsetsGeometry.directional(start: 8, end: 8, bottom: 16),
+                              child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                onTabChange?.call(1, routeName: '/treePage/${poi.id}');
+                              },
+                              child: const Text('Learn More'),
+                              ),
+                            )
+                          ),
+
+                          // close button
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+                              child: ElevatedButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('Close'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
           child: Container(
             decoration: BoxDecoration(
-            color: Colors.orange,
+            color: markerColor,
             shape: BoxShape.circle,
-            border: Border.all(color: Color(0xFFEF6C00), width: 3),
+            border: Border.all(color: Color(markerColor.hashCode - 0x102C00), width: 3),
       ),
           )
         )
